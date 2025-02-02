bdeaae1c6779d75a9949d84a1dc1bedd1c0afe34
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-02 18:41:34 +0200

    Added App icon & Mission View

---
+++ Moonshot/Views/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Amr El-Fiqi on 02/02/2025.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
